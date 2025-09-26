b3bdc262b183cb56fecc2076006207d45bf5fa08
DHIVAN <dhivanarul5@gmail.com>
2025-09-26 09:52:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (14.12%) - LeetSync

---
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int a=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<k){
+                a++;
+            }
+        }
+        return a;
+        
+    }
+}
