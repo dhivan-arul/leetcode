570e0f166202348f56f9fa33bd5d2f4489fe8d15
DHIVAN <dhivanarul5@gmail.com>
2026-06-18 07:17:11 +0530

    Time: 8 ms (91.22%) | Memory: 46.1 MB (43.82%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,5 +1,13 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        return n>0 && 1162261467%n==0;
+      //  return n>0 && 1162261467%n==0;
+      if(n<=0){
+        return false;
+      }
+      long power=1;
+      while(power<n){
+        power=power*3;
+      }
+      return power==n;
     }
 }
