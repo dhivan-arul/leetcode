4ee697e57e69af7ab3f62ac880680a9ac762984c
DHIVAN <dhivanarul5@gmail.com>
2026-07-08 12:19:15 +0530

    Time: 8 ms (10.60%) | Memory: 46.6 MB (8.03%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minLength(String s) {
+        while(s.contains("AB")||s.contains("CD")){
+            s=s.replace("AB","").replace("CD","");
+        }
+        return s.length();
+    }
+}
